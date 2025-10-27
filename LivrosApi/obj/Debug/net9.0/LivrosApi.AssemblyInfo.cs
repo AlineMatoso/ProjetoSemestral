6405b76e2d8ae59a91c55c16c4ff9a2ebe3a1aed
AlineMatoso <alinematosolima@gmail.com>
2025-10-27 18:42:08 -0300

    atualizacao api

--- LivrosApi/obj/Debug/net9.0/LivrosApi.AssemblyInfo.cs
+++ LivrosApi/obj/Debug/net9.0/LivrosApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LivrosApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2608f16ed716bbac7a0dcd8b3f00dd5302765de7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+025ee0048ce128e0258997cf4ec38b33957b4ad0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LivrosApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LivrosApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
