b62a0b12b113a2f2107fc26c4bbfa28c6c91a1c5
AlineMatoso <alinematosolima@gmail.com>
2025-10-27 19:09:10 -0300

    Att

--- LivrosApi/obj/Debug/net9.0/LivrosApi.AssemblyInfo.cs
+++ LivrosApi/obj/Debug/net9.0/LivrosApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LivrosApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6405b76e2d8ae59a91c55c16c4ff9a2ebe3a1aed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a654c18d0efc4164b23cbd85d919336c404df3b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LivrosApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LivrosApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
